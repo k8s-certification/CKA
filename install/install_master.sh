70e1f09da6ec2b931806819c6aea1b424b348f42
eunbong73-lee <85243193+eunbong73-lee@users.noreply.github.com>
2022-10-14 01:31:46 +0900

    Update install_master.sh
    --pod-network-cidr 192.168.0.0/16 추가

--- install/install_master.sh
+++ install/install_master.sh
@@ -59,7 +59,7 @@ systemctl enable kubelet && systemctl start kubelet
 ### init k8s
 rm /root/.kube/config
 kubeadm reset -f
-kubeadm init --kubernetes-version=${KUBE_VERSION} --ignore-preflight-errors=NumCPU --skip-token-print
+kubeadm init --kubernetes-version=${KUBE_VERSION} --ignore-preflight-errors=NumCPU --skip-token-print --pod-network-cidr 192.168.0.0/16
 
 mkdir -p ~/.kube
 sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config
